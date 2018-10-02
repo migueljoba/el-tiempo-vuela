38bf993b7f1eee8b4e6207ee74b0779bd8f4e1a7
Miguel <mjose.baez@gmail.com>
2018-10-01 23:00:12 -0400

    Se agrega audio 'beep' para pocos segundos faltantes.

--- static/js/slider-custom.js
+++ static/js/slider-custom.js
@@ -10,25 +10,28 @@ $sliderTrigger.click(function() {
 	myLoop();
 });
 
-function requestImage() {
-	 $.ajax({
-			 url: '/rand_filename',
-			 dataType: 'json',
-			 success: function(data) {
-					console.log(data);
-					// preparar nueva imagen
-					var $newImg = $('<img>', {src:data.media_url, class:'w-100'});
-					$newImg.css('display', 'none');
 
-					var $container = $('#container-dynamic-img');
-					
-					$container.find('img').fadeOut(1000, function() {
-						$(this).remove();
-						$container.append($newImg);
-						$newImg.fadeIn(1000);
-					});          
-			 }
+function requestImage() {
+	var $ajaxRequest;
+	$ajaxRequest = $.ajax({
+		url: '/rand_filename',
+		dataType: 'json',
 	 });
+
+	$ajaxRequest.done(function(data) {
+		console.log(data);
+		// preparar nueva imagen
+		var $newImg = $('<img>', {src:data.media_url, class:'w-100'});
+		$newImg.css('display', 'none');
+
+		var $container = $('#container-dynamic-img');
+
+		$container.find('img').fadeOut(1000, function() {
+			$(this).remove();
+			$container.append($newImg);
+			$newImg.fadeIn(1000);
+		}); 
+	});
 }
 
 function myLoop() {
